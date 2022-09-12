e21c875388b4e59bf95b7e94d99945e664c86227
Kari Laxman <laxmankari28@gmail.com>
2022-09-12 20:34:16 +0530

    add post with comment feature

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,13 +1,37 @@
+//reference that module to use posts model 
+const Post = require('../models/post');
+
 module.exports.home = function(req,res){
 
-  //try to receive cookie data and print it here
-  console.log(req.cookies);
+  // //try to receive cookie data and print it here
+  // console.log(req.cookies);
 
-  //change the cookie data and you can see in the browser
-  res.cookie('user_id', 25);
-  return  res.render('home', {
-     title : "Home 1"
-  });
+  // //change the cookie data and you can see in the browser
+  // res.cookie('user_id', 25);
+  
+ 
+  // Post.find({}, function(err,posts){
+  //   return res.render('home', {
+  //      title: "Codeial | Home", 
+  //      posts: posts
+  //   })
+  // })
+   
+  //populate the user of each post 
+  Post.find({})
+  .populate('user')
+  .populate({
+    path: 'comments', 
+    populate:{
+      path: 'user'
+    }
+  })
+  .exec(function(err,posts){
+     return res.render('home', {
+       title: "Codeial | Home", 
+       posts: posts
+     });
+  })
 }
 //.home => action Name 
 
